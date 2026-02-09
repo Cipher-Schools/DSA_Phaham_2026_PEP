28dfacdccc62c239e7cfa738150ba8c4c86540e4
Phaham Khan <phaham17@gmail.com>
2026-02-09 10:38:22 +0530

    Added Arrays problems

---
+++ arrays/second_largest_in_array.cpp
@@ -0,0 +1,72 @@
+
+// Iterative - Time Complexity: O(n*log(n) + n) = O(n*log(n))
+int getSecondLargest(vector<int> &arr) {
+    int n = arr.size();
+    
+    // Sort the array in non-decreasing order
+    sort(arr.begin(), arr.end());
+  
+    // start from second last element as last element is the largest
+    for (int i = n - 2; i >= 0; i--) {
+      
+        // return the first element which is not equal to the 
+        // largest element
+        if (arr[i] != arr[n - 1]) {
+            return arr[i];
+        }
+    }
+  
+    // If no second largest element was found, return -1
+    return -1;
+}
+
+// 2nd Approach Without sorting, Time Complexity: O(n)
+
+int getSecondLargest(vector<int> &arr) {
+    int n = arr.size();
+
+    int largest = -1, secondLargest = -1;
+
+    // finding the largest element
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > largest)
+            largest = arr[i];
+    }
+
+    // finding the second largest element
+    for (int i = 0; i < n; i++) {
+
+        // Update second largest if the current element is greater
+        // than second largest and not equal to the largest
+        if (arr[i] > secondLargest && arr[i] != largest) {
+            secondLargest = arr[i];
+        }
+    }
+    return secondLargest;
+}
+
+
+// 3rd Approach - One pass - Time Complexity: O(n)
+int getSecondLargest(vector<int> &arr) {
+    int n = arr.size();
+
+    int largest = -1, secondLargest = -1;
+
+    // finding the second largest element
+    for (int i = 0; i < n; i++) {
+
+        // If arr[i] > largest, update second largest with
+        // largest and largest with arr[i]
+        if(arr[i] > largest) {
+        	secondLargest = largest;
+            largest = arr[i];
+        }
+      
+        // If arr[i] < largest and arr[i] > second largest, 
+        // update second largest with arr[i]
+      	else if(arr[i] < largest && arr[i] > secondLargest) {
+        	secondLargest = arr[i];
+        }
+    }
+    return secondLargest;
+}
